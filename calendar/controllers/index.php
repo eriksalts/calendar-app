811ff0c705d0088bf8ef2caf727ce7ee03c22fb1
vagrant <vagrant@precise64.(none)>
2014-07-30 08:05:11 +0000

    a little styling

--- calendar/controllers/index.php
+++ calendar/controllers/index.php
@@ -7,15 +7,12 @@ class index_controller extends base_controller
 	public function index()
 	{
 
-//        $month_display = date('n');
         $this->month = (int) ($_POST['month'] ? $_POST['month'] : date('n'));
         $this->year = (int)  ($_POST['year'] ? $_POST['year'] : date('Y'));
         $this->monthtext = (int) ($_POST['month'] ? $_POST['month'] : date('F'));
         $this->controls = $this->calendar_controls();
         $this->calendar = $this->draw_calendar();
         $this->calendar_title = $this->draw_calendar_title();
-       // $this->print_notes = $this->print_notes();
-
     }
 
     private function draw_calendar_title(){
@@ -25,91 +22,72 @@ class index_controller extends base_controller
 
     private function calendar_controls(){
 
-        /* date settings */
         $month = (int) ($_POST['month'] ? $_POST['month'] : date('n'));
         $year = (int)  ($_POST['year'] ? $_POST['year'] : date('Y'));
 
-        /* select month control */
         $select_month_control = '<div class="col-lg-4"><select class="form-control" name="month" id="month">';
+
         for($x = 1; $x <= 12; $x++) {
         $select_month_control.= '<option value="'.$x.'"'.($x != $month ? '' : ' selected="selected"').'>'.date('F',mktime(0,0,0,$x,1,$year)).'</option>';
         }
-        $select_month_control.= '</select></div>';
 
-        /* select year control */
+        $select_month_control.= '</select></div>';
         $year_range = 10;
         $select_year_control = '<div class="col-lg-4" id="date-picker-month"><select class="form-control"  name="year" id="year">';
+
         for($x = ($year-floor($year_range/2)); $x <= ($year+floor($year_range/2)); $x++) {
             $select_year_control.= '<option value="'.$x.'"'.($x != $year ? '' : ' selected="selected"').'>'.$x.'</option>';
         }
+
         $select_year_control.= '</select></div>';
-
-        /* "next month" control */
         $next_month_link = '<a href="?month='.($month != 12 ? $month + 1 : 1).'&year='.($month != 12 ? $year : $year + 1).'" class="control">Next Month >></a>';
-
-        /* "previous month" control */
         $previous_month_link = '<a href="?month='.($month != 1 ? $month - 1 : 12).'&year='.($this->month != 1 ? $year : $year - 1).'" class="control"><< 	Previous Month</a>';
+
         $controls ='<div class="container" id="date-picker-year"><div class="row">';
-        /* bringing the controls together */
         $controls .= '<form id="date-picker" method="post">'.$select_month_control.$select_year_control.'<div class="col-lg-4"><input type="submit" id="date-picker-btn" class="btn btn-primary" name="submit" value="Submit" /></div>      '.$previous_month_link.'     '.$next_month_link.' </form>';
         $controls .= '</div></div>';
+
         return $controls;
     }
 
     private function draw_calendar()
     {
-
-        /* draw table */
             $calendar = '<div class="container" id="calendar">';
             $calendar .= '<div id="main-calendar" class="table-responsive"> ';
             $calendar .= '<table cellpadding="0" cellspacing="0" class="calendar table-bordered">';
 
-            /* table headings */
             $headings = array('S','M','T','W','T','F','S');
+
             $calendar.= '<tr class="calendar-row"><td class="calendar-day-head">'.implode('</td><td class="calendar-day-head">',$headings).'</td></tr>';
 
-            /* days and weeks vars now ... */
             $running_day = date('w',mktime(0,0,0,$this->month,1,$this->year));
+
             $days_in_month = date('t',mktime(0,0,0,$this->month,1,$this->year));
+
             $days_in_this_week = 1;
+
             $day_counter = 0;
-            //$dates_array = array();
 
-            /* row for week one */
             $calendar.= '<tr class="calendar-row">';
-
-            /* print "blank" days until the first of the current week */
             for($x = 0; $x < $running_day; $x++):
                 $calendar.= '<td class="calendar-day-np"> </td>';
                 $days_in_this_week++;
             endfor;
-
-
-            /* keep going with days.... */
-                        for($list_day = 1; $list_day <= $days_in_month; $list_day++):
-                            $calendar.= '<td class="calendar-day" id="input-date-' . $list_day . '">';
-                            /* add in the day number */
-                            $calendar.= '<div class="day-number">'.$list_day.'</div>';
-
-
-
+            for($list_day = 1; $list_day <= $days_in_month; $list_day++):
+                $calendar.= '<td class="calendar-day" id="input-date-' . $list_day . '">';
+                $calendar.= '<div class="day-number">'.$list_day.'</div>';
                 $calendar .= '<div class="note-wrapper">';
-                /** QUERY THE DATABASE FOR AN ENTRY FOR THIS DAY !!  IF MATCHES FOUND, PRINT THEM !! **/
+
+               //database query for note
                $sql = 'SELECT * FROM notes WHERE date = "' . $this->year . '-' . $this->month . '-' . $list_day . '";';
                $result = mysql::query('main', $sql);
-                //$debug = $sql;
-
-                foreach ($result as $note) {
-//                   $calendar .= '<div class="note-present"></div>';
-
-                  $calendar .= '<div class="note" id="' . $note['id'] . '">' . $note['title'] . $note['body'];
+                foreach ($result as $note) { $calendar .= '<div class="note" id="' . $note['id'] . '">' . $note['title'] . $note['body'];
                   $calendar .= '<button data-id="' . $note['id'] . '" type="button" class="btn btn-danger delete-button">Delete</button></div>';
                 }
+
                 $calendar .= '</div>';
-
-
-
                 $calendar.= '</td>';
+
                 if($running_day == 6):
                     $calendar.= '</tr>';
                     if(($day_counter+1) != $days_in_month):
@@ -118,29 +96,21 @@ class index_controller extends base_controller
                     $running_day = -1;
                     $days_in_this_week = 0;
                 endif;
+
                 $days_in_this_week++; $running_day++; $day_counter++;
             endfor;
 
-            /* finish the rest of the days in the week */
-            if($days_in_this_week < 8):
-                for($x = 1; $x <= (8 - $days_in_this_week); $x++):
-                    $calendar.= '<td class="calendar-day-np"> </td>';
-                endfor;
-            endif;
+        if($days_in_this_week < 8):
+            for($x = 1; $x <= (8 - $days_in_this_week); $x++):
+                $calendar.= '<td class="calendar-day-np"> </td>';
+            endfor;
+        endif;
 
-            /* final row */
-            $calendar.= '</tr>';
+        $calendar.= '</tr>';
+        $calendar.= '</table></div></div>';
 
-            /* end the table */
-            $calendar.= '</table></div></div>';
-
-            /* all done, return result */
-
-
-            return $calendar;
-
+        return $calendar;
     }
-
 }
 
 
